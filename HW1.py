b55f3fdaf2ad35d21a86e48193d86b3b1ae1c43a
IgorGlush <g2318098@gmail.com>
2022-06-29 12:34:30 +0300

    1 homework 1,2,3 case

--- HW1.py
+++ HW1.py
@@ -1,16 +1,42 @@
-from time import sleep
-import datetime
 
-def outer(funk):
-    def inter(*args,**kwargs):
-        start = datetime.datetime.now()
-        funk()
-        finish = datetime.datetime.now()
-        return finish - start
-    return inter()
+"""декоратор печатающий на экран время работы функции"""
 
-@outer
-def funkt():
-    sleep(5)
+# from time import sleep
+# import datetime
+#
+# def outer(funk):
+#     def inter(*args,**kwargs):
+#         start = datetime.datetime.now()
+#         funk()
+#         finish = datetime.datetime.now()
+#         print (finish - start)
+#     return inter()
+#
+# @outer
+# def funkt():
+#     sleep(5)
+#
+# print(funkt)
 
-print(funkt)
+
+"""функция для вычисления очередного числа Фибоначчи"""
+# def fibb(n):
+#     if n == 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+#     else:
+#         l = [0,1]+[0]*(n-2)
+#         for k in range(2,n):
+#             l[k]=l[k-1]+l[k-2]
+#         return l[n-1]
+#
+# print(fibb(8))
+
+
+"""функция, принимающая 3 позиционных аргумента и возвращающая сумму двух наименьших из них"""
+
+# def summ_min(a,b,c, *args):
+#     return a+b+c-max(a,b,c)
+#
+# print(summ_min(17,23,150,250))
